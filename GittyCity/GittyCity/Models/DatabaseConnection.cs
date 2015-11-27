91efb985c4dbfdc4549a81e73cbc5177104ed208
Ricardo Otte <chucktenton@gmail.com>
2015-11-27 13:28:18 +0100

    DatabaseConnection Class toegevoegd (HEEFT NOG FOUTEN!!!) en mongoDB driver geïnstalleerd.

---
+++ GittyCity/GittyCity/Models/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GittyCity.Models
+{
+    public class DatabaseConnection
+    {
+        public static MongoDatabase databaseConnect()
+        {
+            const string uri = "mongodb://ricardo:hula@145.24.222.168/CityTest";
+            //const string uri = "mongodb://localhost/mydb";
+            var client = new MongoClient(uri);
+            var db = client.GetServer().GetDatabase(new MongoUrl(uri).DatabaseName);
+            return db;
+        }
+    }
+}
