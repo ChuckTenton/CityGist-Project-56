72cbc3badd8450431a1ff4bd5065297f82c51491
Ricardo Otte <chucktenton@gmail.com>
2015-12-21 22:05:49 +0100

    pork

--- GittyCity/GittyCity/Models/DatabaseConnection.cs
+++ GittyCity/GittyCity/Models/DatabaseConnection.cs
@@ -11,11 +11,10 @@ namespace GittyCity.Models
     {
         protected static IMongoClient _client;
         protected static IMongoDatabase _database;
-
         public static IMongoDatabase getMongoDB()
         {
-            _client = new MongoClient("mongodb://145.24.222.168/CityGis"); //Connection string gaat hier
-            //_client = new MongoClient("mongodb://localhost/CityGis");
+            //_client = new MongoClient("mongodb://145.24.222.168/CityGis"); //Connection string gaat hier
+            _client = new MongoClient("mongodb://localhost/CityGis");
             _database = _client.GetDatabase("CityGis");
             return _database;
         }
