7db67b49814c9130f6d81d8aea1da82f48bd661a
0888296 <5314zain@gmail.com>
2016-01-13 10:31:03 +0100

    google maps met locatie marker

--- GittyCity/GittyCity/Scripts/Googlemaps.js
+++ GittyCity/GittyCity/Scripts/Googlemaps.js
@@ -1,10 +1,17 @@
 ﻿function initialize(cor) {
+    var LatLng = new google.maps.LatLng(52.1989253712296, 4.4717221099318);
+
     var mapProp = {
-        center: new google.maps.LatLng(cor),
-        zoom: 5,
+        center: LatLng,
+        zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("google"), mapProp);
-    var map2 = new google.maps.Map(document.getElementById("google2"), mapProp);
+
+    var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map,
+        title: 'Hello World!'
+    });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
