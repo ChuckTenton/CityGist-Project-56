5526df85ea8533927ae0d9849492a0d2b1ca9374
kevke6 <kevke676@gmail.com>
2016-01-19 12:48:10 +0100

    Test met query machine af

--- GittyCity/GittyCity/Models/RaportMaker.cs
+++ GittyCity/GittyCity/Models/RaportMaker.cs
@@ -11,12 +11,21 @@ namespace GittyCity.Models
 {
     public class RaportMaker
     {
-        public static async Task<List<BsonDocument>> getMonitoringFromMongo(string timeStarted, string timeEnded, int id, string date, string wantedTable)
+        public static async Task<List<BsonDocument>> getConnectionFromMongo(string time, int id, string date, string wantedCollection)
         {
             var _database = DatabaseConnection.getMongoDB();
-            var collection = _database.GetCollection<BsonDocument>(wantedTable); ;
+            var collection = _database.GetCollection<BsonDocument>(wantedCollection );
             var builder = Builders<BsonDocument>.Filter;
-            var filter = builder.Eq("BeginTime", date + timeStarted) & builder.Eq("EndTime", date + timeEnded) & builder.Eq("UnitId", id) & builder.Eq("Date", date);
+            var filter = builder.Eq("Time", time) & builder.Eq("UnitId", id) & builder.Eq("Date", date);
+            var results = await collection.Find(filter).ToListAsync();
+            return results;
+        }
+        public static async Task<List<BsonDocument>> getMonitoringFromMongo(string timeStarted, string timeEnded, int id, string date, string type)
+        {
+            var _database = DatabaseConnection.getMongoDB();
+            var collection = _database.GetCollection<BsonDocument>("Monitoring"); ;
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("BeginTime", date + timeStarted) & builder.Eq("EndTime", date + timeEnded) & builder.Eq("UnitId", id) & builder.Eq("Type", type);
             var results = await collection.Find(filter).ToListAsync();
             return results;
         }
